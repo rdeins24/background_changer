461c953ba0ab6a3b04d241cc6e3b4124fc8482da
Denis <rdenis24@yahoo.com>
2022-03-06 11:57:25 +0200

    main

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click',()=>{
+    document.body.style.background = randomBg()
+
+    
+});
+
+function randomBg(){
+    return `hsl(${Math.floor(Math.random()*360)},100%,50%)`;
+}
+
